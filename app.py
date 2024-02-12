56595710bbc52b8c5855324af25b6343702c6960
ricardofig016 <ricardifigueiredo016@gmail.com>
2024-02-12 19:41:32 +0000

    removed decorator functions and the use of the flask g object

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, g, abort
+from flask import Flask, request, jsonify, session, abort
 from functools import wraps
 
 from modules.user import read_user_data, fetch_user_data, store_user_data
@@ -10,74 +10,38 @@ from modules.recommendation import get_recommendations
 app = Flask(__name__)
 
 
-def user_data_decorator(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not hasattr(g, "user_data"):
-            username = request.args.get("username")
-            print(username)
-            if not username:
-                abort(400, "username is required")
-            g.user_data = read_user_data(username)
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
-def media_data_decorator(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not hasattr(g, "media_data"):
-            g.media_data = read_media_data()
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
-def preferences_decorator(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not hasattr(g, "preferences"):
-            g.preferences = get_preferences(g.user_data, g.media_data)
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
-def recommendations_decorator(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not hasattr(g, "recommendations"):
-            g.recommendations = get_recommendations(
-                g.user_data, g.media_data, g.preferences
-            )
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
 @app.route("/")
 def index():
     return "THIS IS THE MAIN PAGE"
 
 
 @app.route("/preferences", methods=["GET"])
-@user_data_decorator
-@media_data_decorator
-@preferences_decorator
 def preferences_route():
-    return jsonify(g.preferences), 200
+    username = request.args.get("username")
+    if not username:
+        return jsonify({"error": "username is required"}), 400
+
+    user_data = read_user_data(username)
+    media_data = read_media_data()
+    preferences = get_preferences(user_data, media_data)
+
+    return jsonify(preferences), 200
 
 
 @app.route("/recommendations", methods=["GET"])
-@user_data_decorator
-@media_data_decorator
-@preferences_decorator
-@recommendations_decorator
 def recommendations_route():
+    username = request.args.get("username")
+    if not username:
+        return jsonify({"error": "username is required"}), 400
+
+    user_data = read_user_data(username)
+    media_data = read_media_data()
+    preferences = get_preferences(user_data, media_data)
+    recommendations = get_recommendations(user_data, media_data, preferences)
+
     size = request.args.get("size", "10")  # Default size is 10
     if size.isdigit() and int(size) > 0 and int(size) <= 1000:
-        return jsonify(g.recommendations[: int(size)]), 200
+        return jsonify(recommendations[: int(size)]), 200
     else:
         return jsonify({"error": "size must be integer and <= 1000"}), 400
 
